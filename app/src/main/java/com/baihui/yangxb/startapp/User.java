f1d7f07129c92c8d71ce2f13df7a60e5a407dc9e
yangxinbin <yangxinbin@hotmail.com>
2017-11-17 09:42:05 +0800

    1.实体类不需要序列化了。

--- app/src/main/java/com/baihui/yangxb/startapp/User.java
+++ app/src/main/java/com/baihui/yangxb/startapp/User.java
@@ -9,7 +9,7 @@ import cn.bmob.v3.datatype.BmobFile;
  * Created by yangxb on 17-10-20.
  */
 
-public class User extends BmobUser implements Serializable { //implements Serializable 用于传递User
+public class User extends BmobUser{
     private BmobFile avatar;
 
     public BmobFile getAvatar() {
